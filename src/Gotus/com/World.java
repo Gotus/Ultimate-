601f8bb71263a933ef9de88a2c438f80304d241c
Gotus <stalnoe5555@mail.ru>
2017-07-22 13:01:32 +0300

    Class World.java was added. It includes(will include) all essentially information to travel for world map.

---
+++ src/Gotus/com/World.java
@@ -0,0 +1,13 @@
+package Gotus.com;
+
+/**
+ * Created by Gotus on 22.07.2017.
+ */
+public class World {
+
+    private String name;
+    private int length;
+    private int height;
+    private int[] worldCells;
+
+}
