770622f47776bc995dc1e63779ac5265998f8d1b
Gotus <stalnoe5555@mail.ru>
2017-07-26 15:46:16 +0300

    Class hich contains information about enemy was added.

---
+++ src/Gotus/com/Enemy.java
@@ -0,0 +1,20 @@
+package Gotus.com;
+
+/**
+ * Created by Gotus on 26.07.2017.
+ */
+public class Enemy {
+
+    String name;
+    int hp;
+    int attack;
+
+    Enemy(String name, int hp, int attack)
+    {
+
+        this.name = name;
+        this.hp = hp;
+        this.attack = attack;
+    }
+
+}
