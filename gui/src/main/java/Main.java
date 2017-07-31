fa4322862a5845f6c9ac787e56eb67bb75db0733
Gotus <stalnoe5555@mail.ru>
2017-07-31 21:53:26 +0300

    Gui module was added to project.

---
+++ gui/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.ultimate.core.*;
+
+import java.util.Scanner;
+
+/**
+ * Created by Gotus on 31.07.2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
