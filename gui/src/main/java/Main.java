53650b96301d921ac2d86bc7b75105d9c0deb0c2
Vladislav Leonov <vlad_leonov@bk.ru>
2017-08-02 21:56:11 +0300

    Add base for GUI

--- gui/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-import com.ultimate.core.*;
-
-
-public class Main {
-
-    public static void main(String[] args) {
-
-    }
-}
---
+++ gui/src/main/java/MainForm.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class MainForm extends JFrame {
+    private JPanel mainPanel;
+
+    private MainForm(){
+        setContentPane(mainPanel);
+        setSize(800, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Ultimate");
+        setLocationRelativeTo(null);
+        //setResizable(false);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MainForm();
+    }
+}
