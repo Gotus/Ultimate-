e756d0b47a48e018458a2352f0c2d4b881a6a4bd
Vladislav Leonov <vlad_leonov@bk.ru>
2017-08-03 13:42:16 +0300

    Adding graphics to MainForm.

--- gui/src/main/java/MainForm.java
+++ gui/src/main/java/MainForm.java
@@ -1,17 +1,33 @@
 import javax.swing.*;
+
+import baseGraphics.ActionScreen;
+import baseGraphics.MainScreen;
+import baseGraphics.SubsidiaryScreen;
 import com.ultimate.core.CurrentGame;
 
-public class MainForm extends JFrame {
-    private JPanel mainPanel;
-    private CurrentGame currentGame;
+import java.awt.*;
 
-    private MainForm(){
-        setContentPane(mainPanel);
-        setSize(800, 600);
+import static baseGraphics.MainScreen.BORDER_SIZE;
+
+public class MainForm extends JFrame {
+    private final static int startWidth = 800, startHeight = 600;
+    private CurrentGame currentGame;
+    private MainScreen mainScreen;
+
+    private MainForm() {
+
+        mainScreen = new MainScreen();
+        mainScreen.setMinimumSize(new Dimension(ActionScreen.MIN_WIDTH + SubsidiaryScreen.WIDTH + 3 * BORDER_SIZE,
+                ActionScreen.MIN_HEIGHT + 2 * BORDER_SIZE));
+        setContentPane(mainScreen);
+
+        pack();
+        setMinimumSize(getSize());
+        setSize(new Dimension(startWidth, startHeight));
+
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Ultimate");
         setLocationRelativeTo(null);
-        //setResizable(false);
         setVisible(true);
     }
 
