c581904d8f8ae904fec8b0b81cd540f0c58c372e
Vladislav Leonov <vlad_leonov@bk.ru>
2017-08-04 18:30:07 +0300

    Adding a simple map visualization

--- gui/src/main/java/baseGraphics/ActionScreen.java
+++ gui/src/main/java/baseGraphics/ActionScreen.java
@@ -2,8 +2,8 @@ package baseGraphics;
 
 
 import com.ultimate.core.CurrentGame;
+import com.ultimate.core.gameObjects.Map;
 
-import javax.rmi.CORBA.Stub;
 import javax.swing.*;
 import java.awt.*;
 
@@ -31,9 +31,31 @@ public class ActionScreen extends JPanel implements IAnimatable, IClickable{
         Graphics2D graphics = (Graphics2D)  g;
 
         //BAD SECTION
-        int cells[][] = baseGraphics.Stub.getMapCells();
+        Map map = baseGraphics.Stub.getMap();
+        int cells[][] = map.getCells();
+        for (int i = 0; i < map.getWidth(); i++) {
+            for (int j = 0; j < map.getHeight(); j++) {
+
+                switch (cells[i][j]) {
+                    case 0:
+                        graphics.setColor(Color.blue);
+                        break;
+                    case 1:
+                        graphics.setColor(Color.green);
+                        break;
+                    default:
+                        graphics.setColor(Color.red);
+                }
+
+                int cellSize = 50, borderSize = 5;
+
+                graphics.fillRect(i * (cellSize + borderSize),
+                                  j * (cellSize + borderSize),
+                                  cellSize, cellSize);
+            }
+        }
         //
-        
+
         graphics.setColor(Color.white);
         graphics.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
         graphics.drawString(getClass().getName(), 4 ,14);
