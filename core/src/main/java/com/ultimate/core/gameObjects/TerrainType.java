cd7c7567ce5d295ac3783a73cf911c761df6b7ae
Vladislav Leonov <vlad_leonov@bk.ru>
2017-08-12 18:50:09 +0300

    Rework TerrainType.

--- core/src/main/java/com/ultimate/core/gameObjects/TerrainType.java
+++ core/src/main/java/com/ultimate/core/gameObjects/TerrainType.java
@@ -13,16 +13,34 @@ public enum TerrainType {
     MOUNTAIN,
     WATER;
 
-    private static HashSet<TerrainType> passable = new HashSet<>(
+    private static HashSet<TerrainType> commonWorldMapTypes = new HashSet<>(
             Arrays.asList(PLAIN, FOREST, DESERT, ICE));
 
     private static Random random = new Random();
 
-    public boolean isPassable() {
-        return passable.contains(this);
+    public static TerrainType getRandom(LocationType locationType) {
+        switch (locationType) {
+            case WORLD:
+                return (TerrainType) commonWorldMapTypes.toArray()[random.nextInt(commonWorldMapTypes.size())];
+            case TOWN:
+                return PLAIN;//TODO change when the commonTownMapTypes will be created
+            case CAVERN:
+                return PLAIN;//TODO change when the commonCavernMapTypes will be created
+            default:
+                return PLAIN;
+        }
     }
 
-    public static TerrainType getRandomPassable() {
-        return (TerrainType) passable.toArray()[random.nextInt(passable.size())];
+    public boolean canMoveThrough(MoveType moveType) {
+        switch (moveType) {
+            case LAND:
+                return ((this != MOUNTAIN) && (this != WATER));
+            case WATER:
+                return (this == WATER);
+            case AIR:
+                return true;
+            default:
+                return false;
+        }
     }
 }
