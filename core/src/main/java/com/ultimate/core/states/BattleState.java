d736a99611172e2e01f87f101f39bfa1fecbf812
Gotus <stalnoe5555@mail.ru>
2017-08-12 16:06:40 +0300

    Attack command implemented.

--- core/src/main/java/com/ultimate/core/states/BattleState.java
+++ core/src/main/java/com/ultimate/core/states/BattleState.java
@@ -39,8 +39,37 @@ public class BattleState implements IState {
 
     public Pair<String, GameState> handleCommand(String... command) {
 
-        //String
+        if (command.length == 0) {
 
+            return new Pair<>("Command not inputed", GameState.WORLD_MAP_STATE);
+        }
+
+        if (command[0].equalsIgnoreCase("attack")) {
+
+            if (command.length < 2) {
+
+                return new Pair<>("Enemy id is not inputed", GameState.BATTLE_STATE);
+            }
+
+            command[1] = command[1].replaceAll("[^\\d]", "");
+
+            if (command[1].equalsIgnoreCase("")) {
+
+                return new Pair<>("The second argument must be number", GameState.WORLD_MAP_STATE);
+            }
+
+            if (attack(Integer.valueOf(command[1]))) {
+
+                return new Pair<>("Victory!", GameState.WORLD_MAP_STATE);
+            }
+
+            return new Pair<>(character.getName() + " " + enemies.get(Integer.valueOf(command[1])).getName() + " " + character.getDamage(), GameState.BATTLE_STATE);
+        }
+
+        if (command[0].equalsIgnoreCase("quit")) {
+
+            System.exit(0);
+        }
         return null;
     }
 }
