bf881dbd34d7705cf3f3cc8af7129edc265a44e1
Gotus <stalnoe5555@mail.ru>
2017-08-13 16:18:30 +0300

    Method for creating character was added.

--- core/src/main/java/com/ultimate/core/states/MenuState.java
+++ core/src/main/java/com/ultimate/core/states/MenuState.java
@@ -7,5 +7,32 @@ import com.ultimate.core.gameObjects.PlayCharacter;
  */
 public class MenuState {
 
-    
+    PlayCharacter newCharacter = new PlayCharacter();
+
+    public String setCharacterName(String data) {
+
+        String fields[] = data.split(" ");
+        if (fields.length < 2) {
+
+            return "Not enough data to create character";
+        }
+
+        if (fields[0].equalsIgnoreCase("") || fields[1].equalsIgnoreCase("")) {
+
+            return "Fields can't be empty";
+        }
+
+        newCharacter.setName(fields[0]);
+
+        for (PlayCharacter.Race race: PlayCharacter.Race.values()) {
+
+            if (race.toString().equalsIgnoreCase(fields[1])) {
+
+                newCharacter.setRace(race);
+                break;
+            }
+        }
+
+        return newCharacter.toString();
+    }
 }
