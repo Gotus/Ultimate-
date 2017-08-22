ffd5b064f47b845cbea3f2b916d6223d3a855a28
Gotus <stalnoe5555@mail.ru>
2017-08-22 20:00:13 +0300

    Transitions between states were added.

--- core/src/main/java/com/ultimate/core/CurrentGame.java
+++ core/src/main/java/com/ultimate/core/CurrentGame.java
@@ -4,6 +4,7 @@ package com.ultimate.core;
 import com.ultimate.core.gameObjects.Location;
 import com.ultimate.core.gameObjects.LocationSize;
 import com.ultimate.core.gameObjects.LocationType;
+import com.ultimate.core.gameObjects.PlayCharacter;
 import com.ultimate.core.states.*;
 import javafx.util.Pair;
 
@@ -14,15 +15,57 @@ public class CurrentGame {
     private GameData gameData;
 
     public CurrentGame() {
-        gameData = new GameData(LocationSize.SMALL, LocationType.WORLD);
+
+        gameData = new GameData(new PlayCharacter("I am", PlayCharacter.Race.HUMAN), new Location(LocationSize.AVERAGE, LocationType.WORLD, 42));
         currentState = new LocationState(gameData.getLocation(), gameData.getPlayCharacter());
+
     }
 
     public String handleCommand(String command){
         //TODO write a body
         String[] parsedCommand = command.split(" ");
-        Pair<String, GameState> commandResult = new Pair<String, GameState>(currentState.handleCommand(parsedCommand).getKey(),
-                currentState.handleCommand(parsedCommand).getValue());
+        Pair<String, GameState> commandResult = currentState.handleCommand(parsedCommand);
+        GameState newState;
+        newState = commandResult.getValue();
+        switch (this.currentState.getState()) {
+
+            case BATTLE_STATE:
+                switch (newState) {
+
+                    case WORLD_MAP_STATE:
+                        this.currentState = new LocationState(gameData.getLocation(), gameData.getPlayCharacter());
+                        break;
+                    case MENU_STATE:
+                        break;
+
+                }
+                break;
+
+            case WORLD_MAP_STATE:
+                switch (newState) {
+
+                    case BATTLE_STATE:
+                        //generate enemies
+                        this.currentState = new BattleState(null, gameData.getPlayCharacter());
+                        break;
+                    case MENU_STATE:
+                        break;
+
+                }
+                break;
+
+            case MENU_STATE:
+                switch (newState) {
+
+                    case WORLD_MAP_STATE:
+                        //generate or load world
+                        break;
+
+                }
+                break;
+        }
+
+
 
         return commandResult.getKey();
     }
