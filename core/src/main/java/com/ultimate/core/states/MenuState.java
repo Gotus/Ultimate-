4acb11993b45dd547536076f9abb87f06e20bae9
Gotus <stalnoe5555@mail.ru>
2017-08-23 20:09:43 +0300

    Now method of creating character checks that another character with same state is not exist.

--- core/src/main/java/com/ultimate/core/states/MenuState.java
+++ core/src/main/java/com/ultimate/core/states/MenuState.java
@@ -5,6 +5,12 @@ import com.sun.org.apache.xpath.internal.operations.Bool;
 import com.ultimate.core.gameObjects.PlayCharacter;
 import javafx.util.Pair;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MenuState implements IState {
 
     public Pair<String, GameState> handleCommand(String command) {
@@ -14,7 +20,41 @@ public class MenuState implements IState {
 
     public Boolean createNewCharacter(PlayCharacter.Race race, String name) {
 
-        PlayCharacter newCharacter = new PlayCharacter(name, race);
+
+        String dirPath = "/saves";
+        Path path = Paths.get(dirPath);
+
+        if (Files.exists(path)) {
+
+            //folder exists
+            File file = new File(dirPath);
+            for (String currentPath:   file.list()) {
+
+                if (currentPath.contains("/" + name + ".name")) {
+
+                    return false;
+                }
+
+                PlayCharacter newCharacter = new PlayCharacter(name, race);
+
+            }
+
+        } else {
+
+            PlayCharacter newCharacter = new PlayCharacter(name, race);
+            File file = new File(dirPath + "/" + name + ".character");
+            file.mkdirs();    //creates /saves folder
+            try {
+
+                file.createNewFile(); //creates file with character information
+
+            } catch (IOException exception) {
+
+                exception.printStackTrace();
+            }
+
+        }
+
         return true;
     }
 
