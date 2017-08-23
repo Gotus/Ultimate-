f67bbe609f983ccb6b49ec6d41f9aa9fd7cfbfb4
Gotus <stalnoe5555@mail.ru>
2017-08-23 19:03:13 +0300

    Now MenuState has method for creating character.

--- core/src/main/java/com/ultimate/core/states/MenuState.java
+++ core/src/main/java/com/ultimate/core/states/MenuState.java
@@ -1,6 +1,8 @@
 package com.ultimate.core.states;
 
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.ultimate.core.gameObjects.PlayCharacter;
 import javafx.util.Pair;
 
 public class MenuState implements IState {
@@ -8,6 +10,12 @@ public class MenuState implements IState {
     public Pair<String, GameState> handleCommand(String command) {
 
         return new Pair<>("TODO", GameState.BATTLE_STATE);//TODO write a body
+    }
+
+    public Boolean createNewCharacter(PlayCharacter.Race race, String name) {
+
+        PlayCharacter newCharacter = new PlayCharacter(name, race);
+        return true;
     }
 
     public GameState getState() {
