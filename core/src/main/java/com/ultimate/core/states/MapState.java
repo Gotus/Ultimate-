f31c79e0838a8dcdcadf43034c59bf34d360d8ac
Gotus <stalnoe5555@mail.ru>
2017-09-05 12:00:48 +0300

    Method for moving to selected node was added.

--- core/src/main/java/com/ultimate/core/states/MapState.java
+++ core/src/main/java/com/ultimate/core/states/MapState.java
@@ -6,14 +6,16 @@ import com.ultimate.core.gameObjects.PlayCharacter;
 import com.ultimate.core.gameObjects.World;
 import javafx.util.Pair;
 
+import java.util.ArrayList;
+
 public class MapState implements IState{
 
-    World location;
+    World currentWorld;
     PlayCharacter character;
 
     public MapState(World location, PlayCharacter character) {
 
-        this.location = location;
+        this.currentWorld = location;
         this.character = character;
     }
 
@@ -27,4 +29,17 @@ public class MapState implements IState{
         return new Pair<>("TODO", GameState.MAP_STATE);//TODO write a body
     }
 
+    Boolean moveToNode(String nodeName) {
+
+        ArrayList<Location> allLoctions = currentWorld.getLocations();
+        for (int i = 0; i < allLoctions.size(); i++) {
+
+            if (nodeName.equalsIgnoreCase(allLoctions.get(i).getName())) {
+                //if pain == life then
+                currentWorld.getCharactersPositions().put(character, new World.CharacterPosition(allLoctions.get(i), allLoctions.get(i).getMap().getStartNode()));
+                return true;
+            }
+        }
+        return false;
+    }
 }
