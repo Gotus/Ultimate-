6fc8698379900d4e1e09efd3c6b9e3da4c4973ca
Gotus <stalnoe5555@mail.ru>
2017-09-10 21:12:56 +0300

    Method for moving in selected direction was added.

--- core/src/main/java/com/ultimate/core/states/MapState.java
+++ core/src/main/java/com/ultimate/core/states/MapState.java
@@ -1,6 +1,7 @@
 package com.ultimate.core.states;
 
 
+
 import com.ultimate.core.gameObjects.Location;
 import com.ultimate.core.gameObjects.Map;
 import com.ultimate.core.gameObjects.PlayCharacter;
@@ -10,6 +11,14 @@ import javafx.util.Pair;
 import java.util.ArrayList;
 
 public class MapState implements IState{
+
+    public enum Direction {
+
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST
+    }
 
     World currentWorld;
     PlayCharacter character;
@@ -35,16 +44,63 @@ public class MapState implements IState{
 
         ArrayList<Map.MapNode> allNodes = currentWorld.getCharactersPositions().get(character).getLocation().getMap().getMapNodes();
         Location currentLocation = currentWorld.getCharactersPositions().get(character).getLocation();
+        World.CharacterPosition currentPosition  = currentWorld.getCharactersPositions().get(character);
 
         for (int i = 0; i < allNodes.size(); i++) {
 
             if (allNodes.get(i).getName().equalsIgnoreCase(nodeName)) {
                 //if pain == life then
                 currentWorld.getCharactersPositions().put(character, new World.CharacterPosition(currentLocation,
-                        allNodes.get(i)));
+                        allNodes.get(i), allNodes.get(i).getX(),allNodes.get(i).getY()));
                 return true;
             }
         }
+        return false;
+    }
+
+    Boolean moveInDirection(Direction direction) {
+
+        World.CharacterPosition currentPosition  = currentWorld.getCharactersPositions().get(character);
+        Map.MapNode currentNode = currentPosition.getMapNode();
+        int currentX = currentNode.getX();
+        int currentY = currentNode.getY();
+        Map currentMap = currentWorld.getCurrentMap();
+
+        switch (direction) {
+
+            case EAST:
+                if (currentX < currentMap.getWidth()) {
+
+                    currentWorld.getCharactersPositions().put(character,
+                            new World.CharacterPosition(currentPosition.getLocation(), currentNode, currentX + 1, currentY));
+                }
+                break;
+
+            case NORTH:
+                if (currentY > 0) {
+
+                    currentWorld.getCharactersPositions().put(character,
+                            new World.CharacterPosition(currentPosition.getLocation(), currentNode, currentX, currentY - 1));
+                }
+                break;
+
+            case SOUTH:
+                if (currentY < currentMap.getHeight()) {
+
+                    currentWorld.getCharactersPositions().put(character,
+                            new World.CharacterPosition(currentPosition.getLocation(), currentNode, currentX, currentY + 1));
+                }
+                break;
+
+            case WEST:
+                if (currentX > 0) {
+
+                    currentWorld.getCharactersPositions().put(character,
+                            new World.CharacterPosition(currentPosition.getLocation(), currentNode, currentX - 1, currentY));
+                }
+                break;
+        }
+
         return false;
     }
 }
